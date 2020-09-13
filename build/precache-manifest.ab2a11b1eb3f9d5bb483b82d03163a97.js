f4e1d817ffa3f6564c040d3d0cfdeea781172178
Sean Kwon <seankwn@live.com>
2020-09-13 18:37:31 -0400

    add build for now

---
+++ build/precache-manifest.ab2a11b1eb3f9d5bb483b82d03163a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d58c724350e96e5dc0e63b3fcd7951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bb04656b771956fa327",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "971f3c84d3f3d24b7ba0",
+    "url": "/static/js/2.b4a604a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4a604a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb04656b771956fa327",
+    "url": "/static/js/main.9ff834a8.chunk.js"
+  },
+  {
+    "revision": "98a9158df4dc7055492e",
+    "url": "/static/js/runtime-main.cb81d975.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
